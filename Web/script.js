82870a21f07146a2ced97ec990386ef8bd17980f
Markus <markusurbanc@gmail.com>
2024-09-14 11:21:07 -0400

    Added code input error message

--- Web/script.js
+++ Web/script.js
@@ -18,3 +18,13 @@ function selection_method_back() {
     document.getElementById("manual_input_form").style.display = "none";
     document.getElementById("selection_buttons").style.display = "block";
 }
+
+
+
+function show_manual_code_error() {
+
+    var classes = "bg-red-50 border border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500 w-full p-4 ps-10 text-sm text-gray-900 border rounded-lg";
+    document.getElementById("manual_pdc_input").className = classes;
+    document.getElementById("manual_pdc_errortext").style.display = "block";
+
+}
