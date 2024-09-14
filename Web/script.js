464d6815e0b2b90c4cc200504692b2d94686bdcf
Markus <markusurbanc@gmail.com>
2024-09-14 11:11:19 -0400

    Added basic JS

---
+++ Web/script.js
@@ -0,0 +1,20 @@
+// FUNCTIONS for Voicemed
+
+// September 2024
+
+function start_scan() {
+    alert("WIP");
+}
+
+
+function manual_input() {
+    // Display the input form
+    document.getElementById("selection_buttons").style.display = "none";
+    document.getElementById("manual_input_form").style.display = "block";
+}
+
+function selection_method_back() {
+    // Display the input form
+    document.getElementById("manual_input_form").style.display = "none";
+    document.getElementById("selection_buttons").style.display = "block";
+}
