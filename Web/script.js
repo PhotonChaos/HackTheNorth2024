a6274bb1b5bbcfe5b18309db295fe920598c4bbe
Markus <markusurbanc@gmail.com>
2024-09-14 20:58:25 -0400

    Conversations bar

--- Web/script.js
+++ Web/script.js
@@ -174,3 +174,28 @@ function clear_conversations() {
     // Clear the conversation bar
     // ...
 }
+
+
+function add_conversation() {
+    // Add a new conversation to the conversation bar
+    var template = `
+    <li>
+        <a href="#" class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group"
+        >
+        <svg class="w-6 h-6 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+            <path fill-rule="evenodd" d="M3.559 4.544c.355-.35.834-.544 1.33-.544H19.11c.496 0 .975.194 1.33.544.356.35.559.829.559 1.331v9.25c0 .502-.203.981-.559 1.331-.355.35-.834.544-1.33.544H15.5l-2.7 3.6a1 1 0 0 1-1.6 0L8.5 17H4.889c-.496 0-.975-.194-1.33-.544A1.868 1.868 0 0 1 3 15.125v-9.25c0-.502.203-.981.559-1.331ZM7.556 7.5a1 1 0 1 0 0 2h8a1 1 0 0 0 0-2h-8Zm0 3.5a1 1 0 1 0 0 2H12a1 1 0 1 0 0-2H7.556Z" clip-rule="evenodd"/>
+        </svg>
+        
+        <span class="ml-3">Paracetamol</span>
+        </a>
+    </li>
+    `;
+    // Create a temporary container to hold the HTML string
+    var temp_container = document.createElement('div');
+
+    // Set the innerHTML of the container to the template
+    temp_container.innerHTML = template;
+
+    document.getElementById("vertical_bar_menu").appendChild(temp_container.firstElementChild);
+
+}
